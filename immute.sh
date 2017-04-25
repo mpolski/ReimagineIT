7ae19951e83f0c8167aea310ea0586aa47beea4c
mpolski <polski@hpe.com>
2017-04-25 13:57:12 +0200

    cleaning up

---
+++ immute.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SERVER=$1
+USER=$2
+PASS=$3
+PROFILE=$4
+#Get the exact profile name from the supplied .json file
+EXTPROFNAME=`cat "$PROFILE" | jq -r .name`
+URL="https://$SERVER"
+
+#Authenticate to HPE OneView API
+AUTH=`curl -s -k -H "X-API-Version: 300" -H "Content-Type: application/json" -d '{"userName":"'$USER'","password":"'$PASS'","loginMsgAck":"true"}' POST $URL/rest/login-sessions | jq -r .sessionID`
+
+#Gets the existing profile names (currently expected to work with 1 profile only) 
+SPNAME=`curl -s -k -H "X-API-Version: 300" -H "Content-Type: application/json" -H "Auth: $AUTH" -X GET $URL/rest/server-profiles | jq -r '.members | .[].name'`
+
+#Compares whether the profile we want to add is already running
+#if yes, then it destroys the profile by calling an external script, waits some seconds for the profile to be destroyed and applies the new version of the profile
+#if no, logs information
+if [ "$SPNAME" == "$EXTPROFNAME" ]; 
+ then
+   echo "Profile $EXTPROFNAME exists!"
+   echo "`./removeServProf.sh $SERVER $USER $PASS $PROFILE $AUTH`";
+   echo "`sleep 85`"
+ else
+   echo "Nothing to destroy, provisioning infrastructure";
+fi
+
---
+++ removeServProf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#chck env varialbes for the first three variables
+SERVER=$1
+USER=$2
+PASS=$3
+JSONFILE=$4
+AUTH=$5
+URL="https://$SERVER"
+
+EXTPROFNAME=`cat "$JSONFILE" | jq -r .name`
+
+
+#AUTH=`curl -s -k -H "X-API-Version: 300" -H "Content-Type: application/json" -d '{"userName":"'$USER'","password":"'$PASS'","loginMsgAck":"true"}' POST $URL/rest/login-sessions | jq -r .sessionID`
+
+echo $SERVER $USER $PASS $JSONFILE
+echo " "
+echo $AUTH
+echo ""
+echo "Removing profile "$EXTPROFNAME
+curl -s -k -H "X-API-Version: 300" -H "Content-Type: application/json" -H "Auth: $AUTH" -X DELETE $URL/rest/server-profiles?filter=name="'$EXTPROFNAME'"
